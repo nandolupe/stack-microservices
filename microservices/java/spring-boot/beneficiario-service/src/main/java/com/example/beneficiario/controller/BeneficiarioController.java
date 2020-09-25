18bf96e821e7be44a58c906fd49ec4d9942f3b81
Luiz Fernando Pereira <nandolupe@gmail.com>
2020-09-25 20:16:08 -0300

    backup dos arquivos

--- microservices/java/spring-boot/beneficiario-service/src/main/java/com/example/beneficiario/controller/BeneficiarioController.java
+++ microservices/java/spring-boot/beneficiario-service/src/main/java/com/example/beneficiario/controller/BeneficiarioController.java
@@ -3,6 +3,7 @@ package com.example.beneficiario.controller;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.beneficiario.model.Beneficiario;
 
+@RefreshScope
 @RestController
 public class BeneficiarioController {
-	
+
 	@Autowired
 	private Environment env;
 	
-	 @RequestMapping(value = "/buscar", method = RequestMethod.GET, produces = "application/json") 
-	 
-	 public Beneficiario buscarBeneficiario() { Beneficiario
-		 beneficiario = new Beneficiario("Luiz Fernando Pereira", new Date(), "M", env.getProperty("teste.propriedade"));
-		 
-		 return beneficiario; 
-	 }
+	@Value("${teste.propriedade}")
+	private String valorTeste;
+	
+	@RequestMapping(value = "/buscar", method = RequestMethod.GET, produces = "application/json")
+	public Beneficiario buscarBeneficiario() {
+		
+		Beneficiario beneficiario = new Beneficiario("Luiz Fernando Pereira", 
+				new Date(), 
+				"M", 
+				env.getProperty("teste.propriedade") + " Valor de Classe: " + valorTeste);
+
+		return beneficiario;
+	}
 
 }
